aa7ca16b8994bbac5c631a222b5766b266c08fef
Naman Shahh <88183206+naman3010@users.noreply.github.com>
2026-01-14 19:11:10 +0530

    Remove pip install statement for streamlit
    Removed pip install statement for streamlit.

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1kzQl0cEnfN7bYisgN-h1Ydbk7I45s1Jq
 """
 
-pip install streamlit
-
 import streamlit as st
 import numpy as np
 import joblib
